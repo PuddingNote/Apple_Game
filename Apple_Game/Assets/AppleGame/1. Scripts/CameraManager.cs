5a9eb8333347f110e336eecd592af5eafc0d5adc
Pudding <gkscksgma14@gmail.com>
2025-02-19 16:17:17 +0900

    refactor: CameraManager 코드 리팩토링
    1. 화면비 변수 상수로 선언 : 메모리 최적화 및 값이 변경되지 않음을 표시
    2. FindObjectOfType > GerComponent 사용 : 불필요한 검색 제거로 인한 성능 향상
    3. 불필요한 싱글톤 삭제

--- Apple_Game/Assets/AppleGame/1. Scripts/CameraManager.cs
+++ Apple_Game/Assets/AppleGame/1. Scripts/CameraManager.cs
@@ -3,36 +3,21 @@ using UnityEngine;
 // 카메라 화면 비율을 16:9으로 고정하는 스크립트
 public class CameraManager : MonoBehaviour
 {
-    public static CameraManager Instance { get; private set; }
-
-    private float targetAspect = 16f / 9f;
+    private const float TARGET_ASPECT = 16f / 9f;
+    private Camera mainCamera;
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-    }
-
-    private void Start()
-    {
+        mainCamera = GetComponent<Camera>();
         InitializeCameraSystem();
     }
 
     private void InitializeCameraSystem()
     {
-        Camera cam = FindObjectOfType<Camera>();
         float currentAspect = (float)Screen.width / Screen.height;
-        float scaleHeight = currentAspect / targetAspect;
+        float scaleHeight = currentAspect / TARGET_ASPECT;
 
-        Rect rect = cam.rect;
+        Rect rect = mainCamera.rect;
 
         if (scaleHeight < 1)
         {
@@ -46,6 +31,8 @@ public class CameraManager : MonoBehaviour
             rect.x = (1f - scaleWidth) * 0.5f;
         }
 
-        cam.rect = rect;
+        mainCamera.rect = rect;
     }
+
+
 }
